6c6d7b5fb8a4cc7c86701b42fb801a9768c7ff84
cartoonboxx <shulenin_2005@mail.ru>
2025-03-07 23:54:40 +0300

    v5.4

--- miniapp/config/prizes_app/models.py
+++ miniapp/config/prizes_app/models.py
@@ -21,13 +21,17 @@ class UserPrizeModel(models.Model):
     invited_from = models.IntegerField(verbose_name='Приглашен от айди', blank=True, null=True)
     photo = models.CharField(verbose_name='Фото', max_length=150)
 
-    def collect_users_invites(self):
+    prize_id = models.IntegerField(verbose_name='Айди розыгрыша')
+
+    @staticmethod
+    def collect_users_invites(user_id):
         return UserPrizeModel.objects.filter(
-            invited_id=self.user_id
+            invited_from=user_id
         )
 
-    def calc_len_invites(self):
-        return len(self.collect_users_invites())
+    @staticmethod
+    def calc_len_invites(user_id):
+        return len(UserPrizeModel.collect_users_invites(user_id))
 
     def __str__(self):
         return self.name
@@ -35,3 +39,37 @@ class UserPrizeModel(models.Model):
     class Meta:
         verbose_name = 'Пользователь конкурса'
         verbose_name_plural = 'Пользователи конкурсов'
+
+class UserWinnersPrizeModel(UserPrizeModel):
+
+    def calc_user_chance(self):
+        base_weight = 1
+        coefficient = 1
+        participants = [{
+            'user_id': user.user_id,
+            'invites': UserPrizeModel.calc_len_invites(user.user_id)
+        } for user in UserPrizeModel.objects.all()]
+
+        total_weight = sum(
+            base_weight + participant['invites'] * coefficient
+            for participant in participants
+        )
+
+        result = []
+        for participant in participants:
+            weight = base_weight + participant['invites'] * coefficient
+            chance = (weight / total_weight) * 100 if total_weight else 0  # avoid division by zero
+
+            result.append({
+                **participant,  # Copy existing participant data
+                'weight': weight,
+                'chance': chance,
+            })
+
+        for user in result:
+            if user.get('user_id') == self.user_id:
+                current_chance = user.get('chance')
+                return current_chance
+
+        return 0
+
